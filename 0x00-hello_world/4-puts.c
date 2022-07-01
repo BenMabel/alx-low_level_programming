41b89869fb3f7c3fdb34160a6a07e8ae9181d75e
BenMabel <itzmabella103@gmail.com>
2022-06-30 19:25:58 -0700

    Task 2

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point of code
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
